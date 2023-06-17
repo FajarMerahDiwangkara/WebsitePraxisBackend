744dcc03e1f2529a02605c7a73c5a64137a3f8f1
Fajar Merah Diwangkara <fajarmerah2020@mail.ugm.ac.id>
2023-06-17 17:30:27 +0700

    removed comments

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from secret import secret_data
 
 # config and start server
 from server import app, csrf
-# https://www.pascallandau.com/blog/git-secret-encrypt-repository-docker/#adding-and-encrypting-files
 app.config['SECRET_KEY'] = secret_data['SECRET_KEY']
 app.config['databasewebsitepraxis_username'] = \
 secret_data['databasewebsitepraxis_username']
