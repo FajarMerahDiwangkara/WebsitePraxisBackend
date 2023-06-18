6b82d88e2ce8446df5e158665f27017b17dca7f2
Fajar Merah Diwangkara <fajarmerah2020@mail.ugm.ac.id>
2023-06-18 16:15:15 +0700

    added register system, account password not hashed yet, added database dump

--- config.py
+++ config.py
@@ -3,5 +3,15 @@ config_data = {
     "databasewebsitepraxis_port" : "5432",
     "databasewebsitepraxis_schema" : "public",
     "user_login_session_timeout_seconds" : 3600,
-    "timestamp_format" : "%Y-%m-%d %H:%M:%S"
+    "timestamp_format" : "%Y-%m-%d %H:%M:%S",
+    "account_name_max_length" : 320,
+    # https://stackoverflow.com/questions/9289451/regular-expression-for-alphabets-with-spaces
+    "account_name_regex" : "[a-zA-Z][a-zA-Z ]+",
+    "account_password_max_length" : 320,
+    # email max length is 320, search on internet
+    "account_email_max_length" : 320,
+    "account_address_max_length" : 1000,
+    "account_education_max_length" : 1000,
+    "account_phone_number_regex" : "^[+0-9]+$",
+    "account_phone_number_max_length" : 15
 }
--- controller/routes.py
+++ controller/routes.py
@@ -6,6 +6,7 @@ from http import HTTPStatus
 from flask_wtf.csrf import generate_csrf, validate_csrf
 
 import math
+import re
 
 from model import database_connection   
 from datetime import date, datetime
@@ -18,26 +19,170 @@ def home_page():
 @app.route('/login', methods=['GET'])
 @app.route('/login/', methods=['GET'])
 def login_page():
-    if verify_login_status():
-        return redirect('/')
-    else:
-        return login_page_function()
+    return login_page_function()
 
 @app.route('/login/submit', methods=['POST'])
 def login_submit():
     return login_submit_function()
+
+@app.route('/register', methods=['GET'])
+@app.route('/register/', methods=['GET'])
+def register_page():
+    return register_page_function()
+
+@app.route('/register/submit', methods=['POST'])
+def register_submit():
+    return register_submit_function()
 
 # route function
 
 def home_page_function():
     return response.ok("")
 
-def login_page_function():
+def register_page_function():
+    response_data = {}
+    response_code = HTTPStatus.OK.value
+    if 'session' not in request.cookies:
+        # call flask_wtf.csrf.generate_csrf() to force flask-wtf to generate and set session cookie to user
+        generate_csrf()
+    register_form_csrf_token = generate_csrf()
+    response_data["register_form_csrf_token"] = register_form_csrf_token
+    response = make_response(jsonify(response_data))
+    # remove existing login_session_cookie
+    response.set_cookie('login_session_uuid', "", 0, 0)
+    return response,response_code
+
+def register_submit_function():
+    response_data = {
+        "data_complete" : None,
+        "csrf_token_valid" : None,
+        "name_valid" : None,
+        "password_valid" : None,
+        "email_valid" : None,
+        "email_already_exist" : None,
+        "address_valid" : None,
+        "education_valid" : None,
+        "phone_number_valid" : None
+    }
+    response_code = HTTPStatus.OK.value
+    # verify user sent all the data
+    response_data['data_complete'] = False
+    if 'name' in request.form and 'password' in request.form and 'email' in request.form and 'address' in request.form \
+        and 'education' in request.form and 'phone_number' in request.form \
+        and 'register_form_csrf_token' in request.form and 'session' in request.cookies:
+        response_data['data_complete'] = True
+        response_data['csrf_token_valid'] = False
+        validate_csrf(request.form['register_form_csrf_token'])
+        response_data['csrf_token_valid'] = True
+        # https://stackoverflow.com/questions/181530/styling-multi-line-conditions-in-if-statements
+        response_data['name_valid'] = False
+        if (len(request.form['name']) == 0 or request.form['name'].isspace() or
+            len(request.form['name']) > app.config['account_name_max_length'] or 
+            not re.match(app.config['account_name_regex'], request.form['name'])):
+            response_data['error_log'] = "Account name invalid, either contain invalid character, is empty string, or too long."
+            response_code = HTTPStatus.BAD_REQUEST.value
+            return make_response(jsonify(response_data)), response_code
+        response_data['name_valid'] = True
+        response_data['password_valid'] = False
+        if (len(request.form['password']) == 0 or request.form['password'].isspace() or 
+            len(request.form['password']) > app.config['account_password_max_length']):
+            response_data['error_log'] = "Account password invalid, either contain invalid character, is empty string, or too long."
+            response_code = HTTPStatus.BAD_REQUEST.value
+            return make_response(jsonify(response_data)), response_code
+        response_data['password_valid'] = True
+        # From https://stackoverflow.com/questions/16454939/regex-to-validate-international-email
+        # Regex to verify email format which include international domain email is too complicated/long/impractical, 
+        # the current best way to check whether email is valid or not is to try to send verification mail to the email
+        response_data['email_valid'] = False
+        if (len(request.form['email']) == 0 or request.form['email'].isspace() or
+            len(request.form['email']) > app.config['account_email_max_length']):
+            response_data['error_log'] = "Account email invalid, either contain invalid character, is empty string, or too long."
+            response_code = HTTPStatus.BAD_REQUEST.value
+            return make_response(jsonify(response_data)), response_code
+        response_data['email_valid'] = True
+        response_data['email_already_exist'] = True
+        db_conn = database_connection.get_postgres_connection(
+                app.config['databasewebsitepraxis_username'],
+                app.config['databasewebsitepraxis_password'],
+                app.config['databasewebsitepraxis_host'],
+                app.config['databasewebsitepraxis_port'],
+                "databasewebsitepraxis"
+            )
+        with db_conn.cursor() as db_cursor:
+            sql_query = "SELECT * FROM " + app.config['databasewebsitepraxis_schema'] + ".account_data WHERE email = %s"
+            db_cursor.execute(sql_query, (
+                request.form['email'],
+            ))
+            db_conn.commit()
+            account_data = db_cursor.fetchone()
+            if account_data is not None:
+                response_data['error_log'] = "Account email already exist, unable to register."
+                response_code = HTTPStatus.CONFLICT.value
+                return make_response(jsonify(response_data)), response_code
+        response_data['email_already_exist'] = False
+        response_data['address_valid'] = False
+        if (len(request.form['address']) == 0 or request.form['address'].isspace() or
+            len(request.form['address']) > app.config['account_address_max_length']):
+            response_data['error_log'] = "Account address invalid, either is empty string, or too long."
+            response_code = HTTPStatus.BAD_REQUEST.value
+            return make_response(jsonify(response_data)), response_code
+        response_data['address_valid'] = True
+        response_data['education_valid'] = False
+        if (len(request.form['education']) == 0 or request.form['education'].isspace() or
+            len(request.form['education']) > app.config['account_education_max_length']):
+            response_data['error_log'] = "Account education invalid, either is empty string, or too long."
+            response_code = HTTPStatus.BAD_REQUEST.value
+            return make_response(jsonify(response_data)), response_code
+        response_data['education_valid'] = True
+        response_data['phone_number_valid'] = False
+        if (len(request.form['phone_number']) == 0 or request.form['phone_number'].isspace() or
+            len(request.form['phone_number']) > app.config['account_phone_number_max_length'] or 
+            not re.match(app.config['account_phone_number_regex'], request.form['phone_number'])):
+            response_data['error_log'] = "Account phone number invalid, either contain invalid character, is empty string, or too long."
+            response_code = HTTPStatus.BAD_REQUEST.value
+            return make_response(jsonify(response_data)), response_code
+        response_data['phone_number_valid'] = True
+        
+        # at this stage, the data is valid and email does not exist, register account
+        with db_conn.cursor() as db_cursor:
+            sql_query = """INSERT INTO """ + app.config['databasewebsitepraxis_schema'] + \
+                """.account_data VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+            db_cursor.execute(sql_query, (
+                str(uuid4()),
+                request.form['name'],
+                # TODO
+                # Hash password
+                request.form['password'],
+                request.form['email'],
+                request.form['address'],
+                request.form['education'],
+                request.form['phone_number'],
+                # TODO
+                # Email verification, for now set email verified True
+                True,
+                datetime.strftime(datetime.now(), app.config['timestamp_format'])
+            ))
+            db_conn.commit()
+        return make_response(jsonify(response_data)), response_code
+    else:
+        response_data['error_log'] =  "Some required account data or csrf token or session cookie is missing"
+        return make_response(jsonify(response_data)), HTTPStatus.BAD_REQUEST.value
+    
+def register_success():
     response_data = {}
     response_code = HTTPStatus.OK.value
 
-    # call flask_wtf.csrf.generate_csrf() to force flask-wtf to generate and set session cookie to user
-    generate_csrf(token_key="login_form")
+    return make_response(jsonify(response_data)), response_code
+
+def login_page_function():
+    # verify user isnt logged in
+    if verify_login_status():
+        return redirect('/')
+    response_data = {}
+    response_code = HTTPStatus.OK.value
+    if 'session' not in request.cookies:
+        # call flask_wtf.csrf.generate_csrf() to force flask-wtf to generate and set session cookie to user
+        generate_csrf()
     login_form_csrf_token = generate_csrf()
     response_data['login_form_csrf_token'] = login_form_csrf_token
     response = make_response(jsonify(response_data))
@@ -46,13 +191,15 @@ def login_page_function():
     return response,response_code
 
 def login_submit_function():
-    # https://www.geeksforgeeks.org/hashing-passwords-in-python-with-bcrypt/
     # verify user isnt logged in
     if verify_login_status():
         return redirect('/')
     response_data = {}
     response_code = HTTPStatus.OK.value
-    # verify data exist
+    if 'session' not in request.cookies:
+        # call flask_wtf.csrf.generate_csrf() to force flask-wtf to generate and set session cookie to user
+        generate_csrf()
+    # verify user sent all the data
     if 'email' in request.form and 'password' in request.form and 'login_form_csrf_token' in request.form and 'session' in request.cookies:
         validate_csrf(request.form['login_form_csrf_token'])
         email = request.form['email']
@@ -65,7 +212,7 @@ def login_submit_function():
                 "databasewebsitepraxis"
             )
         with db_conn.cursor() as db_cursor:
-            sql_query = "SELECT * FROM " + app.config['databasewebsitepraxis_schema'] + ".account_data WHERE email = %s and account_password = %s;"
+            sql_query = "SELECT * FROM " + app.config['databasewebsitepraxis_schema'] + ".account_data WHERE email = %s and password = %s;"
             db_cursor.execute(sql_query, (
                 email,
                 password
@@ -80,13 +227,18 @@ def login_submit_function():
                 response.set_cookie('login_session_uuid', session_data['login_session_uuid'])
                 return response, response_code
             else:
-                response_data['error'] = "Email or password is incorrect"
+                response_data['error_log'] = "Email or password is incorrect"
                 return make_response(jsonify(response_data)), HTTPStatus.UNAUTHORIZED.value
     else:
-        response_data['error'] =  "Email or password or csrf token or session cookie is missing"
+        response_data['error_log'] =  "Email or password or csrf token or session cookie is missing"
         return make_response(jsonify(response_data)), HTTPStatus.BAD_REQUEST.value
 
 # working function
+
+def hash_password(password, account_creation_timestamp):
+    # https://www.geeksforgeeks.org/hashing-passwords-in-python-with-bcrypt/
+    # TODO
+    pass
 
 def verify_login_status():
     # check if user logged in
@@ -128,7 +280,6 @@ def generate_new_login_session_data(account_uuid, session_token):
         app.config['databasewebsitepraxis_port'],
         "databasewebsitepraxis"
     )
-    print("running one!")
     with db_conn.cursor() as db_cursor:
         sql_query = "INSERT INTO " + app.config['databasewebsitepraxis_schema'] + ".account_login_session VALUES (%s, %s, %s, %s, %s);"
         db_cursor.execute(sql_query, (
